5f28dbd141b8a7af3a2ce0b99da0f125735d6e84
vidyasagar1793 <vidyasagar994356@gmail.com>
2024-01-08 12:43:02 +0530

    committed

---
+++ Range Sum of BST.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+    void solve(TreeNode* root,int low,int high,int &sum){
+        if(root==NULL)
+            return; 
+        
+        if(root->val<=high && root->val>=low)
+            sum=sum+root->val;
+
+        solve(root->left,low,high,sum);
+        solve(root->right,low,high,sum);
+        
+     
+    }
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum=0;
+        solve(root,low,high,sum);
+        return sum;
+    }
+};
