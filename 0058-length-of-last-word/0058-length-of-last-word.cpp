81dca3199d260151bda950ba334358816284ceb1
Vidya sagar <110242938+vidyasagar1793@users.noreply.github.com>
2024-04-01 15:13:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-          int count=0;
-        for(int i=s.size()-1;i>=0;i--){
-            if(s[i]!=' '){
+        int count =0;
+      int n=s.size();
+        for(int i=s.size()-1;i>0;i--){
+            
+           if(s[i]==' '){
+            if(count>0) {
+                return count;
+            }
+           }
+
+           else{
             count++;
-            }
-            if(s[i]==' '){
-                if (count>0){
-                    break;
-                }
-            }
+           }
         }
-        return count; 
+   return count;
+
     }
 };
